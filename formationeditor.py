3596110f4be366a90f64307874767a989b0b0ba4
Michael Yeiter <Aceplace1116@gmail.com>
2018-10-27 12:23:38 -0400

    Can drag players around in formation visualizer.

--- formationeditor.py
+++ formationeditor.py
@@ -10,21 +10,98 @@ class FormationEditor(Frame):
         player_edit_frame = Frame(self, padx = 10, pady = 10)
         player_edit_frame.pack()
         Label(player_edit_frame, text="T").grid(row=0, column=0)
-        t_entry = Entry(player_edit_frame).grid(row=0, column=1)
+        self.t_entry = Entry(player_edit_frame)
+        self.t_entry.grid(row=0, column=1)
         Label(player_edit_frame, text="H").grid(row=1, column=0)
-        h_entry = Entry(player_edit_frame).grid(row=1, column=1)
+        self.h_entry = Entry(player_edit_frame)
+        self.h_entry.grid(row=1, column=1)
         Label(player_edit_frame, text="X").grid(row=2, column=0)
-        x_entry = Entry(player_edit_frame).grid(row=2, column=1)
+        self.x_entry = Entry(player_edit_frame)
+        self.x_entry.grid(row=2, column=1)
         Label(player_edit_frame, text="Y").grid(row=3, column=0)
-        y_entry = Entry(player_edit_frame).grid(row=3, column=1)
+        self.y_entry = Entry(player_edit_frame)
+        self.y_entry.grid(row=3, column=1)
         Label(player_edit_frame, text="Z").grid(row=4, column=0)
-        z_entry = Entry(player_edit_frame).grid(row=4, column=1)
+        self.z_entry = Entry(player_edit_frame)
+        self.z_entry.grid(row=4, column=1)
         Label(player_edit_frame, text="Q").grid(row=5, column=0)
-        q_entry = Entry(player_edit_frame).grid(row=5, column=1)
+        self.q_entry = Entry(player_edit_frame)
+        self.q_entry.grid(row=5, column=1)
+        update_button = Button(player_edit_frame, text="Update Positions", command=lambda:self.update_positions())
+        update_button.grid(row=6, column=0, columnspan=2)
+
+        self.current_formation = Formation()
 
         formation_visualizer_frame = Frame(self)
         formation_visualizer_frame.pack(fill=BOTH, expand=TRUE)
-        formation_visualizer = FormationVisualizer(formation_visualizer_frame, Formation())
+        self.formation_visualizer = FormationVisualizer(formation_visualizer_frame)
+        self.formation_visualizer.arrange_players_in_formation(self.current_formation)
+
+        self.update_entry_boxes_with_coordinates()
+
+
+        frame = Frame(root, bd=2, relief=SUNKEN)
+
+
+
+
+    def update_positions(self):
+        self.update_position(self.t_entry, 'T')
+        self.update_position(self.h_entry, 'H')
+        self.update_position(self.x_entry, 'X')
+        self.update_position(self.y_entry, 'Y')
+        self.update_position(self.z_entry, 'Z')
+        self.update_position(self.q_entry, 'Q')
+
+        self.formation_visualizer.arrange_players_in_formation(self.current_formation)
+        self.update_entry_boxes_with_coordinates()
+
+    def update_position(self, entry, label):
+        coordinate_text = entry.get()
+        #convert coordinates to numbers
+        try:
+            split_text = coordinate_text.split(',')
+            x,y = None, None
+
+            if len(split_text) != 2:
+                raise ValueError('Coordinates not in proper format')
+            x = int(split_text[0])
+            y = int(split_text[1])
+
+            self.current_formation.players[label].x = x
+            self.current_formation.players[label].y = y
+        except Exception: #Any exceptions will simply cause us to use the coordinates already in place
+            pass
+
+
+
+    def update_entry_boxes_with_coordinates(self):
+        self.t_entry.delete(0, END)
+        self.t_entry.insert(0, self.get_string_for_player_coordinates('T'))
+        self.h_entry.delete(0, END)
+        self.h_entry.insert(0, self.get_string_for_player_coordinates('H'))
+        self.x_entry.delete(0, END)
+        self.x_entry.insert(0, self.get_string_for_player_coordinates('X'))
+        self.y_entry.delete(0, END)
+        self.y_entry.insert(0, self.get_string_for_player_coordinates('Y'))
+        self.z_entry.delete(0, END)
+        self.z_entry.insert(0, self.get_string_for_player_coordinates('Z'))
+        self.q_entry.delete(0, END)
+        self.q_entry.insert(0, self.get_string_for_player_coordinates('Q'))
+
+
+    def get_string_for_player_coordinates(self, label):
+        return str(self.current_formation.players[label].x) + ',' + str(self.current_formation.players[label].y)
+
+
+
+
+
+
+
+
+
+
 
 
 
--- formationvisualizer.py
+++ formationvisualizer.py
@@ -21,45 +21,124 @@ RIGHT_TUP_OF_NUMBERS = CENTER_X_POS + HORIZONTAL_COORDINATE_SIZE * 35
 RIGHT_BOTTOM_OF_NUMBERS = CENTER_X_POS + HORIZONTAL_COORDINATE_SIZE * 39
 FIVE_YARDS = VERTICAL_COORDINATE_SIZE * 5
 
-class FormationVisualizer(Canvas):
-    def __init__(self, root, formation):
-        Canvas.__init__(self, root)
-        self.configure(background="white")
-        self.pack(fill=BOTH, expand=True)
+class FormationVisualizer(Frame):
+    def __init__(self, root):
+        Frame.__init__(self, root)
+
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
+
+        xscrollbar = Scrollbar(self, orient=HORIZONTAL)
+        xscrollbar.grid(row=1, column=0, sticky=E+W)
+        yscrollbar = Scrollbar(self)
+        yscrollbar.grid(row=0, column=1, sticky=N+S)
+
+        self.canvas = Canvas(self, bd=0, xscrollcommand=xscrollbar.set, background='white',
+                        yscrollcommand=yscrollbar.set, scrollregion=(0,0,1200,800))
+        self.canvas.grid(row=0, column=0, sticky=N+S+E+W)
+        xscrollbar.config(command=self.canvas.xview)
+        yscrollbar.config(command=self.canvas.yview)
+
         self.draw_field_lines()
-        self.draw_players_in_formation(formation)
+        self.create_players_for_formation()
+
+        self.pack(fill=BOTH, expand=True)
+
+        self.drag_data = {"x": 0, "y": 0, "item": None}
+        self.canvas.bind("<ButtonPress-1>", self.on_press)
+        self.canvas.bind("<ButtonRelease-1>", self.on_release)
+        self.canvas.bind("<B1-Motion>", self.on_move)
+
+
 
     def draw_field_lines(self):
         #draw sideline
-        self.create_line(LEFT_SIDELINE, CENTER_Y_POS - FIVE_YARDS * 3, LEFT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * 2)
-        self.create_line(RIGHT_SIDELINE, CENTER_Y_POS - FIVE_YARDS * 3, RIGHT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * 2)
+        self.canvas.create_line(LEFT_SIDELINE, CENTER_Y_POS - FIVE_YARDS * 3, LEFT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * 2)
+        self.canvas.create_line(RIGHT_SIDELINE, CENTER_Y_POS - FIVE_YARDS * 3, RIGHT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * 2)
 
         for num in range(-3, 3):
             #draw lines that go accross field at 5 yard intervals
-            self.create_line(LEFT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * num, RIGHT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * num)
+            self.canvas.create_line(LEFT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * num, RIGHT_SIDELINE, CENTER_Y_POS + FIVE_YARDS * num)
             #draw hash marks
-            self.create_line(LEFT_HASH, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, LEFT_HASH, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
-            self.create_line(RIGHT_HASH, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, RIGHT_HASH, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
+            self.canvas.create_line(LEFT_HASH, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, LEFT_HASH, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
+            self.canvas.create_line(RIGHT_HASH, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, RIGHT_HASH, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
             #draw ticks for the top of numbers and bottom of numbers
-            self.create_line(LEFT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, LEFT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
-            self.create_line(LEFT_TOP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, LEFT_TOP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
-            self.create_line(RIGHT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, RIGHT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
-            self.create_line(RIGHT_TUP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, RIGHT_TUP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
+            self.canvas.create_line(LEFT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, LEFT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
+            self.canvas.create_line(LEFT_TOP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, LEFT_TOP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
+            self.canvas.create_line(RIGHT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, RIGHT_BOTTOM_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
+            self.canvas.create_line(RIGHT_TUP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS - HASH_SIZE, RIGHT_TUP_OF_NUMBERS, CENTER_Y_POS + num * FIVE_YARDS + HASH_SIZE)
 
-    def draw_players_in_formation(self, formation):
+    def create_players_for_formation(self):
+        self.player_shapes = {}
+        formation = Formation() #create default formation to place players around in
         for label, player in formation.players.items():
             x, y = FormationVisualizer.player_coordinates_to_canvas(player)
-            self.create_oval(x - PLAYER_WIDTH / 2, y - PLAYER_HEIGHT / 2, x + PLAYER_WIDTH / 2, y + PLAYER_HEIGHT / 2,
-                            fill="white")
-            self.create_text(x, y, text=player.label, font=LABEL_FONT)
+            self.player_shapes[label] = {"Oval" : self.canvas.create_oval(x - PLAYER_WIDTH / 2, y - PLAYER_HEIGHT / 2, x + PLAYER_WIDTH / 2, y + PLAYER_HEIGHT / 2, fill="white"),
+                                        "Label" : self.canvas.create_text(x, y, text=player.label, font=LABEL_FONT)}
 
+    def arrange_players_in_formation(self, formation):
+        for label, player in formation.players.items():
+            x, y = FormationVisualizer.player_coordinates_to_canvas(player)
+            self.canvas.coords(self.player_shapes[label]["Oval"], x - PLAYER_WIDTH / 2, y - PLAYER_HEIGHT / 2, x + PLAYER_WIDTH / 2, y + PLAYER_HEIGHT / 2)
+            self.canvas.coords(self.player_shapes[label]["Label"], x, y)
 
     @staticmethod
     def player_coordinates_to_canvas(player):
         return (CENTER_X_POS + player.x * HORIZONTAL_COORDINATE_SIZE, CENTER_Y_POS + player.y * VERTICAL_COORDINATE_SIZE)
 
+
+    def on_press(self, event): #get initial location of object to be moved
+        x = self.canvas.canvasx(event.x)
+        y = self.canvas.canvasy(event.y)
+        drag_items = self.canvas.find_overlapping(x-1, y-1, x+1, y+1)
+        if drag_items:
+            player_drag_item = self.select_player_to_drag(drag_items[0])
+            self.drag_data["item"] = player_drag_item
+            self.drag_data["x"] = x
+            self.drag_data["y"] = y
+        else:
+            self.drag_data["item"] = None
+            self.drag_data["x"] = 0
+            self.drag_data["y"] = 0
+
+    def on_release(self, event): #reset data on release
+        self.drag_data["item"] = None
+        self.drag_data["x"] = 0
+        self.drag_data["y"] = 0
+
+    def on_move(self, event):
+        if self.drag_data["item"]:
+            x = self.canvas.canvasx(event.x)
+            y = self.canvas.canvasy(event.y)
+            delta_x = x - self.drag_data["x"]
+            delta_y = y - self.drag_data["y"]
+            # move the object the appropriate amount
+            if abs(delta_x) > HORIZONTAL_COORDINATE_SIZE:
+                move_x = int(delta_x / HORIZONTAL_COORDINATE_SIZE) * HORIZONTAL_COORDINATE_SIZE
+                self.canvas.move(self.drag_data["item"]["Oval"], move_x, 0)
+                self.canvas.move(self.drag_data["item"]["Label"], move_x, 0)
+                self.drag_data["x"] = self.drag_data["x"] + move_x
+            if abs(delta_y) > VERTICAL_COORDINATE_SIZE:
+                move_y = int(delta_y / VERTICAL_COORDINATE_SIZE) * VERTICAL_COORDINATE_SIZE
+                self.canvas.move(self.drag_data["item"]["Oval"], 0, move_y)
+                self.canvas.move(self.drag_data["item"]["Label"], 0, move_y)
+                self.drag_data["y"] = self.drag_data["y"] + move_y
+            #self.canvas.move(self.drag_data["item"], delta_x, delta_y)
+            # record the new position
+            #self.drag_data["x"] = x
+            #self.drag_data["y"] = y
+
+    def select_player_to_drag(self, drag_item):
+        for player in self.player_shapes.values():
+            if drag_item is player["Oval"] or drag_item is player["Label"]:
+                return player
+        return None
+
 if __name__ == "__main__":
     root = Tk()
-    formation = Formation()
-    fvc = FormationVisualizer(root, formation)
+    fvc = FormationVisualizer(root)
+    flipped_formation = Formation()
+    flipped_formation.flip_formation()
+    fvc.arrange_players_in_formation(flipped_formation)
     root.mainloop()
